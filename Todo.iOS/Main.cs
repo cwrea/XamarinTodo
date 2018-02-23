7591d271e9af75851ac7103f7e8b82f2ab4cdf4c
Chris W. Rea <chris@dynasphere.ca>
2018-02-23 09:53:10 -0500

    Updated EF Core from 1.1.5 to 2.0.1

--- Todo.iOS/Main.cs
+++ Todo.iOS/Main.cs
@@ -7,6 +7,8 @@ namespace Todo
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
+			SQLitePCL.Batteries_V2.Init(); // required for iOS after moving to EF Core v2
+
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main(args, null, "AppDelegate");
